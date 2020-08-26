a8882cd87c25caeaf60f2b765b9ee6adf0acb947
jibin <jokercsi.gmail.com>
2020-08-26 21:02:53 +0900

    react-native-loginpage

---
+++ components/Mainscreen.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+class Mainscreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+export default Mainscreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
